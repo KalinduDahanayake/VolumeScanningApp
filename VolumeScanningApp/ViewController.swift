38223f927d80437c641966b59a7f1d23ad4fbd1d
KalinduDahanayake <kalindu.dahanayake2@gmail.com>
2022-01-23 21:18:57 +1100

    Initial Commit

---
+++ VolumeScanningApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VolumeScanningApp
+//
+//  Created by Kalindu Dahanyake on 23/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
